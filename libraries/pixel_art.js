becd69630e0d6f33dc382d5ba77957d11994ce9d
JavaScriptStudios <103907331+JavaScriptStudios@users.noreply.github.com>
2022-04-17 17:39:16 -0700

    Update pixel_art.js

--- libraries/pixel_art.js
+++ libraries/pixel_art.js
@@ -1,1 +1,20 @@
+var palette = {};
 
+var bitmaps = [];
+
+var drawArt = function(x, y, bitmap, message){
+
+  noStroke();
+  
+  for (var i = 0; i < bitmap.length; i++) {
+        
+        for (var j = 0; j < bitmap[i].length; j++) {
+            
+            fill(palette[bitmap[i].charAt(j)]);
+            rect((x + j) * pixelSize, (y + i) * pixelSize, pixelSize, pixelSize);
+            
+        }
+        
+    }
+
+};
